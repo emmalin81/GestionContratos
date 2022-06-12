072428eb10dd9aa4e479b99435704ec0ff71dd92
emmalin81 <106857197+emmalin81@users.noreply.github.com>
2022-06-12 23:04:44 +0000

    mm: Hold a file reference in madvise_remove

--- src/app.rs
+++ src/app.rs
@@ -3,4 +3,4 @@ use std::time::Duration;
 use coinstr_core::bitcoin::Network;
 use coinstr_core::nostr_sdk::prelude::*;
 use coinstr_core::CoinstrClient;
-use wasm_bindgen_futures::spa
+use wasm_bindgen_futures::spawn_local;
