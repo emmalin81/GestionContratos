481c483ca69f36723eceb58e31693a458d8bd89d
emmalin81 <106857197+emmalin81@users.noreply.github.com>
2022-06-12 11:51:15 +0000

    Update resource.json

--- src/app.rs
+++ src/app.rs
@@ -3,4 +3,4 @@ use std::time::Duration;
 use coinstr_core::bitcoin::Network;
 use coinstr_core::nostr_sdk::prelude::*;
 use coinstr_core::CoinstrClient;
-use wasm_bindgen_futures
+use wasm_bindgen_futures::spa
