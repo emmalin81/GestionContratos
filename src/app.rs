11a7078478ef0b3e5692d3e763148d27d643d81e
emmalin81 <106857197+emmalin81@users.noreply.github.com>
2022-06-14 13:07:17 +0000

    small typo fixes

--- src/app.rs
+++ src/app.rs
@@ -4,4 +4,4 @@ use coinstr_core::bitcoin::Network;
 use coinstr_core::nostr_sdk::prelude::*;
 use coinstr_core::CoinstrClient;
 use wasm_bindgen_futures::spawn_local;
-use yew::prel
+use yew::prelude:
