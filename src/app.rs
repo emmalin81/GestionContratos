044bf3103dd000364fe846486467742c3a2c664f
emmalin81 <106857197+emmalin81@users.noreply.github.com>
2022-06-18 13:19:14 +0000

    added blog

--- src/app.rs
+++ src/app.rs
@@ -4,4 +4,6 @@ use coinstr_core::bitcoin::Network;
 use coinstr_core::nostr_sdk::prelude::*;
 use coinstr_core::CoinstrClient;
 use wasm_bindgen_futures::spawn_local;
-use yew::prelude:
+use yew::prelude::*;
+
+const 
