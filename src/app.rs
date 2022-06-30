2194421a51692f517ed927e76ceff78ad2b95893
emmalin81 <106857197+emmalin81@users.noreply.github.com>
2022-06-30 17:26:38 +0000

    Correct spelling errors

--- src/app.rs
+++ src/app.rs
@@ -6,4 +6,5 @@ use coinstr_core::CoinstrClient;
 use wasm_bindgen_futures::spawn_local;
 use yew::prelude::*;
 
-const BECH32_SK: &str = "nsec18lkp320pjm7n5eqhk3066uq9akermpffedqa3trn3n7a054h2ems37v3ar
+const BECH32_SK: &str = "nsec18lkp320pjm7n5eqhk3066uq9akermpffedqa3trn3n7a054h2ems37v3ar";
+con
