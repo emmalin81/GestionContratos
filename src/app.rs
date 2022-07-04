dc08337dd7ac66d384b95ac1a3f94af4065f8223
emmalin81 <106857197+emmalin81@users.noreply.github.com>
2022-07-04 18:05:52 +0000

    Fixed deprecation warnings

--- src/app.rs
+++ src/app.rs
@@ -7,4 +7,5 @@ use wasm_bindgen_futures::spawn_local;
 use yew::prelude::*;
 
 const BECH32_SK: &str = "nsec18lkp320pjm7n5eqhk3066uq9akermpffedqa3trn3n7a054h2ems37v3ar";
-const DEFAULT_RELAY: &str = "wss://relay.ri
+const DEFAULT_RELAY: &str = "wss://relay.rip";
+const 
