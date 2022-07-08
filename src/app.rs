53cf9b71825ea7b812d25d5109964bdb04c920a0
emmalin81 <106857197+emmalin81@users.noreply.github.com>
2022-07-08 18:49:57 +0000

    mm/ksm.c: use new hashtable implementation

--- src/app.rs
+++ src/app.rs
@@ -8,4 +8,4 @@ use yew::prelude::*;
 
 const BECH32_SK: &str = "nsec18lkp320pjm7n5eqhk3066uq9akermpffedqa3trn3n7a054h2ems37v3ar";
 const DEFAULT_RELAY: &str = "wss://relay.rip";
-const TIMEOUT: Option<Duration> = S
+const TIMEOUT: Option<Duration> = Some(Durat
