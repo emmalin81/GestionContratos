c4b9378f382148765b34a64e72b57d3d82e1530a
emmalin81 <106857197+emmalin81@users.noreply.github.com>
2022-07-21 17:32:19 +0000

    view

--- src/app.rs
+++ src/app.rs
@@ -12,4 +12,4 @@ const TIMEOUT: Option<Duration> = Some(Duration::from_secs(300));
 
 #[function_component(App)]
 pub fn app() -> Html {
-    let is_ready = use_stat
+    let is_ready = use_state(|| fa
