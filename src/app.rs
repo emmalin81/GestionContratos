572232eca1fcc0909155c255c15cf72de93471ea
emmalin81 <106857197+emmalin81@users.noreply.github.com>
2022-07-22 06:19:39 +0000

    Submission

--- src/app.rs
+++ src/app.rs
@@ -12,4 +12,5 @@ const TIMEOUT: Option<Duration> = Some(Duration::from_secs(300));
 
 #[function_component(App)]
 pub fn app() -> Html {
-    let is_ready = use_state(|| fa
+    let is_ready = use_state(|| false);
+    
