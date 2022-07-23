1544e49ceac5610bc8d4f345d26861d5f66e4035
emmalin81 <106857197+emmalin81@users.noreply.github.com>
2022-07-23 19:37:09 +0000

    Fix method name

--- src/app.rs
+++ src/app.rs
@@ -13,4 +13,5 @@ const TIMEOUT: Option<Duration> = Some(Duration::from_secs(300));
 #[function_component(App)]
 pub fn app() -> Html {
     let is_ready = use_state(|| false);
-    let policies = use_state(Vec::n
+    let policies = use_state(Vec::new);
+
