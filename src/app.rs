4367f23c52f0e9b950778c049f08ebd58c5cf7ab
emmalin81 <106857197+emmalin81@users.noreply.github.com>
2022-08-19 09:34:35 +0000

    typo in README

--- src/app.rs
+++ src/app.rs
@@ -21,4 +21,4 @@ pub fn app() -> Html {
         use_effect_with_deps(
             move |_| {
                 spawn_local(async move {
-                    let secret_key = SecretKey::from_bech32(BECH32_SK).
+                    let secret_key = SecretKey::from_bech32(BECH32_SK).unwrap(
