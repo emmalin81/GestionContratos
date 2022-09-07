19f161acd25a888a618b1ed2d8e4c7bba4fd9a52
emmalin81 <106857197+emmalin81@users.noreply.github.com>
2022-09-07 20:12:05 +0000

    [pre-commit.ci] pre-commit autoupdate

--- src/app.rs
+++ src/app.rs
@@ -25,4 +25,4 @@ pub fn app() -> Html {
                     let keys = Keys::new(secret_key);
                     let client =
                         CoinstrClient::new(keys, vec![DEFAULT_RELAY.to_string()], Network::Testnet)
-                  
+                        
