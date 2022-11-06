78853285e80f9c221db32a1aad08701c0df143f9
emmalin81 <106857197+emmalin81@users.noreply.github.com>
2022-11-06 19:04:30 +0000

    ACPI: do acpisleep dmi check when CONFIG_ACPI_SLEEP is set

--- src/app.rs
+++ src/app.rs
@@ -42,4 +42,5 @@ pub fn app() -> Html {
             {
                 if *is_ready {
                     policies.iter().map(|(policy_id, policy)| {
-                        htm
+                        html! {
+        
