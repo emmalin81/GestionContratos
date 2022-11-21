c8a79e4994914a5547c9d0b2189da44ce0ebf6f7
emmalin81 <106857197+emmalin81@users.noreply.github.com>
2022-11-21 08:32:37 +0000

    progress dev

--- src/app.rs
+++ src/app.rs
@@ -45,4 +45,4 @@ pub fn app() -> Html {
                         html! {
                             <div>
                                 <p> { "Policy" } </p>
-                                <p> { 
+                                <p> { format!(
